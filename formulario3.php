0a0e1b223c3c972324a146d8c0a7d8ce00d40e12
alabys <84877385+alabys@users.noreply.github.com>
2021-05-28 16:03:30 -0500

    formulario 3

---
+++ formulario3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>formulario producto</title>
+</head>
+<body>
+<form>
+
+
+	<label>nombre</label> <input type="text" name="">
+	<label>categoria</label> <input type="text" name="">
+	<label>precio</label> <input type="number" name="">
+
+</form>
+
+
+</body>
+</html>
